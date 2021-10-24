f44f3eda8dad4aa12901230b299c758c6eb3aa1b
Atharva Nandanwar <atharva.nandanwar@colorado.edu>
2021-10-24 10:54:01 -0600

    setup: get data metric

--- setup/docker/setup.sh
+++ setup/docker/setup.sh
@@ -14,13 +14,13 @@ cd "${dl_dir}"
 mkdir -p /usr/local/cross-compiler
 
 echo -n "Downloading source tarballs..."
-wget https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz > /dev/null 2>&1 || true
-wget https://ftpmirror.gnu.org/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz > /dev/null 2>&1 || true
-wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.1.tar.gz > /dev/null 2>&1 || true
-wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.bz2 > /dev/null 2>&1 || true
-wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz > /dev/null 2>&1 || true
-wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2 > /dev/null 2>&1 || true
-wget https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz > /dev/null 2>&1 || true
+wget https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz
+wget https://ftpmirror.gnu.org/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz
+wget https://ftpmirror.gnu.org/mpfr/mpfr-4.0.1.tar.gz
+wget https://ftpmirror.gnu.org/gmp/gmp-6.1.2.tar.bz2
+wget https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz
+wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2
+wget https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
 echo "done!"
 
 echo -n "Extract the tarballs..."
